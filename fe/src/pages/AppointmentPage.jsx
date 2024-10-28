ac6dae9f17192db12e3eeaa22bf34a8d8790d2de
Phuoc Nguyen <139487477+PhuocNgu47@users.noreply.github.com>
2024-10-28 09:23:13 +0700

    update Request Appointment

--- fe/src/pages/AppointmentPage.jsx
+++ fe/src/pages/AppointmentPage.jsx
@@ -1,11 +1,9 @@
-import "../styles/Appointment.css";
+import '../styles/Appointment.css'
 
 function AppointmentPage() {
     return (
         <div className="wrapper-appoinment">
-            <div className="title-apm">
-                Request Appointment
-            </div>
+            <div className="title-apm">Request Appointment</div>
             <div className="title-apm-innet">
                 <span>If you have a pressing concern, please call our office directly at (831) 278-1081.</span>
                 <span>Our phones and emails are answered 9am-3pm PST Monday through Saturday, but are closed most major holidays.</span>
@@ -40,26 +38,25 @@ function AppointmentPage() {
                     </label>
                     <label>
                         Description (500 characters or less)
-                        <textarea maxLength="500" className="login-input" style={{ height: "192px" }} />
+                        <textarea maxLength="500" className="login-input" style={{ height: '192px' }} />
                     </label>
                     <div className="capcha">
                         <div className="title-capcha">CAPTCHA</div>
                         <label className="captcha-label">
-                            
-                            <div style={{display:"flex", gap: "10px", alignItems:"center"}}> 
-                            <input type="checkbox" className="captcha-checkbox" required />
-                            Im not a robot
-                            
+                            <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+                                <input type="checkbox" className="captcha-checkbox" required />
+                                Im not a robot
                             </div>
                             <img src="/src/assets/images/logo-capcha.png" alt="" width="58px" height="50px" />
-                            
                         </label>
                     </div>
-                    <button type="submit" className="login-button">Submit</button>
+                    <button type="submit" className="login-button">
+                        Submit
+                    </button>
                 </form>
             </div>
         </div>
-    );
+    )
 }
 
-export default AppointmentPage;
+export default AppointmentPage
