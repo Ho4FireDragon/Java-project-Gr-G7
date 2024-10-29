560bb08bc55f371757704e6d3d5b52e81ce637cf
Dong-1234 <2251120010@ut.edu.vn>
2024-10-29 21:26:46 +0700

    add file giao dien

---
+++ giao dien/Dịch vụ/custom.js
@@ -0,0 +1,5 @@
+// Custom JavaScript for handling service booking
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listeners or any other initialization code here
+  });
+  
---
+++ giao dien/Tài khoản/custom.js
@@ -0,0 +1,5 @@
+// Custom JavaScript for handling login and registration
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listeners or any other initialization code here
+  });
+  
