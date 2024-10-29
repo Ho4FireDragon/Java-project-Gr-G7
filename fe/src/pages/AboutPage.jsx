be49bec719b1d3b4a76c89a32d7426775c405238
NguyenThanhYYY <2251120199@ut.edu.vn>
2024-10-29 21:50:11 +0700

    thêm 'AboutPage'

--- fe/src/pages/AboutPage.jsx
+++ fe/src/pages/AboutPage.jsx
@@ -1,5 +1,154 @@
-function AboutPage() {
-    return <div>Hello About Page</div>
-}
+import React from 'react'
 
-export default AboutPage
+export default function AboutPage() {
+    return (
+        <div>
+            {' '}
+            <div className="header_aboutUs">
+                <h1 className="text_Header">About us</h1>
+            </div>
+            <div className="first_Part">
+                <img src="https://cafishvet.com/wp-content/uploads/2020/06/09-19-18-jessie-portraits-0134-e1552397731333.jpg" alt="" />
+                <p className="paragraph_Content">
+                    <b className="Jessie_Sanders">Jessie Sanders, DVM, DABVP (Fish Practice) - Chief Veterinarian </b>
+                    <br />
+                    <div className="content_Two">
+                        Dr. Sanders is the owner and Chief Veterinarian of <a href=""> Aquatic Veterinary Services.</a> She attended Tufts University for her DVM and holds a B.S. in Marine Biology
+                        from the University of Rhode Island. She is a <a href="">certified aquatic veterinarian </a> through the <a href=""> World Aquatic Veterinary Medical Association. </a> She is
+                        one of the first board certified veterinarians in fish practice through the <a href="#"> American Board of Veterinary Practitioners.</a>
+                    </div>{' '}
+                    <div className="content_Two">
+                        Dr. Sanders was the 2020 President of the American Association of Fish Veterinarians and served as the Private Practice chair on the Aquatic Veterinary Medicine Committee of
+                        the American Veterinary Medical Association. Dr. Sanders is a frequent speaker at many national veterinary conferences.
+                    </div>
+                    <div className="content_Two">
+                        With USDA Level II Accreditation, Dr. Sanders is able to sign off on Certificates of Veterinary Inspection (CVIs) for fish entering or leaving the country and Veterinary Feed
+                        Directives (VFDs) providing medicated feed for fish in need.
+                    </div>
+                    <div className="content_Two">
+                        More information about Dr. Sanders is available <a href=""> on her website. Click here to read about how she got started in aquatic veterinary medicine.</a>
+                    </div>
+                </p>
+            </div>
+            <div className="second_Part">
+                <div className="second_Part_Content">
+                    <div className="main_Content">
+                        <div className="content_Wrapper">
+                            <p>
+                                <div className="practice_Manager">Sara, Practice Manager</div>
+                                Sara is our practice manager and has been with Aquatic Veterinary Services since 2015. She started out volunteering to assist with different surgical procedures as our
+                                anesthesiologist. Sara is an expert when it comes to fish husbandry and is the voice of Aquatic Veterinary Services. <br />{' '}
+                                <div className="content_Two">Sara enjoys spending her free time with her husband, son and two dogs. </div>
+                            </p>
+                            <img src="https://cafishvet.com/wp-content/uploads/2020/11/Sara-Enos-scaled.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="Third_Part">
+                <div className="Third_Part_Content">
+                    <div className="main_Content_Three">
+                        <div className="content_Wrapper_Three">
+                            <img src="https://cafishvet.com/wp-content/uploads/2020/10/Talking-On-The-Phone1-.png" alt="" />
+                            <p>
+                                <div className="practice_Manager_Three">Adam, Weekend Manager</div>
+                                <div className="content_Two">
+                                    Adam is our weekend manager and manages our phones and scheduling. Joining us in September 2019 to cover Sara’s maternity leave, we just couldn’t let him leave! He
+                                    takes the time to seamlessly transition our staff from week to week while managing his own salsa company. <br />{' '}
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="fourth_Part">
+                <div className="fourth_Part_Content">
+                    <div className="main_Content_Fourth">
+                        <div className="content_Wrapper_Fourth">
+                            <p>
+                                <div className="practice_Manager_Fourth">Our Veterinary Partners Aquatic Pet Vet - Southern California</div>
+                                <div className="content_Two">
+                                    For our clients in Santa Barbara, Ventura and Los Angeles counties, we have partnered with Dr. Christina Dover, CertAqV of the Aquatic Pet Vet. She offers mobile
+                                    veterinary service for fish and is excellent at what she does! <br />{' '}
+                                </div>
+                            </p>
+                            <img src="https://cafishvet.com/wp-content/uploads/2024/03/APVstraightlogoblackcropped.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="fifth_Part">
+                <button>Learn More</button>
+                <img src="https://cafishvet.com/wp-content/uploads/2024/03/1706666891325.jpg" alt="" />
+            </div>
+            <div className="sixth_Part">
+                <div className="sixth_Part_Content">
+                    <div className="main_Content_sixth">
+                        <div className="content_Wrapper_Sixth">
+                            <img src="https://cafishvet.com/wp-content/uploads/2014/09/usda-logo.gif" alt="" />
+                            <p>
+                                <div className="practice_Manager">Aquatic Veterinary Services is an Accredited Veterinarian with the USDA</div>
+                                Aquatic Veterinary Services can help you with overseas exports (CVI) and medicated feed (VFD)! Dr. Jessie Sanders is a Level II Accredited Veterinarian with the USDA.
+                                We are able to sign off on all aquatic products for shipment overseas. If you have a specific request in mind, please contact us at (831) 278-1081.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="our-story-container">
+                <div className="text-content">
+                    <h2 className="title">Our Story</h2>
+                    <p className="intro">One of my colleagues likes to start all of his speaking engagements by asking the following question:</p>
+
+                    <blockquote className="quote">
+                        <p>
+                            If you walk up to any random person on the street and ask them for <a href="#help">help with your pet fish</a>, what will they tell you to do?
+                        </p>
+                        <footer>A. David Scarfe PhD, DVM, MRSSAf, CertAqV</footer>
+                    </blockquote>
+
+                    <div className="answers">
+                        <h3>Here are the top answers:</h3>
+                        <ol>
+                            <li>Look it up on the internet.</li>
+                            <li>Ask the people at the pet store.</li>
+                            <li>Flush it.</li>
+                        </ol>
+                    </div>
+                </div>
+
+                <div className="image-content">
+                    <img src="https://cafishvet.com/wp-content/uploads/2018/07/img_5693.jpg" alt="Vet with fish bag" />
+                </div>
+            </div>
+            <div>
+                <div className="fish-care-container">
+                    <div className="content-section">
+                        <div className="text-content">
+                            <p>
+                                No where on this list does it mention a veterinarian! To be honest, most veterinarians wouldn’t know what to do with a fish if it came through their door.
+                                <a href="#pet-fish">Pet fish</a> are a severely under-served collection of pets. Fish do not receive the same respect as a cat, dog, or horse. Any pet parent has
+                                squandered hours trying to find help for a simple problem or severe illness or injury. But what if there was someone who could answer your questions, fix your fish
+                                quickly with minimal stress, and prevent it from happening again? Well, here we are.
+                            </p>
+                            <p>
+                                Aquatic Veterinary Services is <a href="#dedicated">dedicated to treating pet fish</a> like family. Fish deserve all the same care and consideration as your other pets,
+                                and our service is your one-stop shop. We are here to make your fish-keeping lives as easy as possible with expert advice and assistance without mucking about in
+                                internet forums or pet store after pet store.
+                            </p>
+                        </div>
+                        <div className="image-content">
+                            <img src="https://cafishvet.com/wp-content/uploads/2018/03/sck-portraits-8169-edit-913x1024.jpg" alt="Veterinarian with fish" />
+                        </div>
+                    </div>
+
+                    <div className="cta-section">
+                        <h2>Schedule Your Fish Exam</h2>
+                        <p>Schedule an appointment or call us at (831) 278-1081.</p>
+                        <button className="cta-button">Request Appointment</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
